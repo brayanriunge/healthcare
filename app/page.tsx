029137c2c7f70592b43e69e1745c4a71e15db7b7
RiugeBrian <riungebrayandev@gmail.com>
2024-07-11 16:23:48 +0300

    adding public folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <p>Home</p>;
+  return (
+    <div className="h-screen flex max-h-screen">
+      <div className=" container my-auto remove-scrollbar">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="h-10 mb-12 w-fit"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
