a7d15bdb7a7b09b40da1cfd0a6252be5e65d1ed3
RiugeBrian <riungebrayandev@gmail.com>
2024-07-12 10:17:22 +0300

    custom form feild

--- components/Form/PatientForm.tsx
+++ components/Form/PatientForm.tsx
@@ -4,16 +4,19 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+import { Form } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import CustomFormField from "../CustomForm/CustomFormFeild";
+
+export enum FormFieldType {
+  INPUT = "input",
+  CHECKBOX = "checkbox",
+  TEXTAREA = "textarea",
+  DATE_PICKER = "datePicker",
+  PHONE_INPUT = "phoneInput",
+  SELECT = "select",
+  SKELETON = "skeleton",
+}
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -36,22 +39,22 @@ export default function PatientForm() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-6 flex-1 "
+      >
+        <div className="mb-12 space-y-4">
+          <h1 className="header">Hi there</h1>
+          <p className="text-dark-700">Schedule your first appointment</p>
+        </div>
+        <CustomFormField
           control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
+          fieldType={FormFieldType.INPUT}
+          name="name"
+          label="Full Name"
+          placeHolder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
         />
         <Button type="submit">Submit</Button>
       </form>
