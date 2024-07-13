8e7269a0a064cf43db8e77296c27f21702f78bdb
RiugeBrian <riungebrayandev@gmail.com>
2024-07-13 17:43:11 +0300

    action creating user

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,8 +1,15 @@
-import { Query } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { users } from "../appwrite.config";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
+    const newUser = await users.create(
+      ID.unique(),
+      user.email,
+      user.name,
+      user.phone,
+      undefined
+    );
   } catch (error: any) {
     if (error && error?.code === 409) {
       const documents = await users.list([Query.equal("email", [user.email])]);
