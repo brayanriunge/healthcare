43debd401de1b16a89e294c1e38453f274567116
RiugeBrian <riungebrayandev@gmail.com>
2024-07-16 11:18:01 +0300

    rgisterForm

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,20 +1,37 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Register = () => {
   return (
-    <div className="h-screen max-h-screen flex">
-      <section className="remove-scrollbar container">
-        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
+    <div className="h-screen flex max-h-screen">
+      <div className=" container my-auto remove-scrollbar">
+        <div className="sub-container max-w-[496px]">
           <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
-            alt="carepulse"
-            className="mb-12 h-10  w-fit"
+            alt="patient"
+            className="h-10 mb-12 w-fit"
           />
+          {/* <PatientForm /> */}
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
-      </section>
+      </div>
+      <Image
+        src="/assets/images/register-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[390px]"
+      />
     </div>
   );
 };
