d93366c6ece0bf7bdeb3cad90f886f98b11be10b
RiugeBrian <riungebrayandev@gmail.com>
2024-07-18 16:55:14 +0300

    checkbox custom

--- components/CustomForm/CustomFormField.tsx
+++ components/CustomForm/CustomFormField.tsx
@@ -23,6 +23,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import { Select, SelectContent, SelectTrigger } from "../ui/select";
 import { SelectValue } from "@radix-ui/react-select";
 import { Textarea } from "../ui/textarea";
+import { Checkbox } from "@radix-ui/react-checkbox";
 
 interface CustomProps {
   control: Control<any>;
@@ -134,6 +135,19 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
             </SelectContent>
           </Select>
         </FormControl>
+      );
+    case FormFieldType.CHECKBOX:
+      return (
+        <div className="flex items-center gap -4">
+          <Checkbox
+            id={props.name}
+            checked={field.value}
+            onCheckedChange={field.onChange}
+          />
+          <label htmlFor={props.name} className="checkbox-label">
+            {props.label}
+          </label>
+        </div>
       );
     default:
       return null;
