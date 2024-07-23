1f6b17c6869acbc77d7894c57796f08c4a620dc4
RiugeBrian <riungebrayandev@gmail.com>
2024-07-23 15:31:02 +0300

    appointment page design

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -6,7 +6,7 @@ export default function NewAppointment() {
   return (
     <div className="h-screen flex max-h-screen">
       <div className=" container my-auto remove-scrollbar">
-        <div className="sub-container max-w-[496px]">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
           <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
@@ -14,23 +14,18 @@ export default function NewAppointment() {
             alt="patient"
             className="h-10 mb-12 w-fit"
           />
-          <PatientForm />
-          <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 CarePulse
-            </p>
-            <Link href="/?admin=true" className="text-green-500">
-              Admin
-            </Link>
-          </div>
+
+          <p className="justify-items-end text-dark-600 xl:text-left">
+            © 2024 CarePulse
+          </p>
         </div>
       </div>
       <Image
-        src="/assets/images/onboarding-img.png"
+        src="/assets/images/appointment-img.png"
         height={1000}
         width={1000}
-        alt="patient"
-        className="side-img max-w-[50%]"
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
       />
     </div>
   );
