4b84aba52aed8b6ed07c5fe1c629add341faf4cf
RiugeBrian <riungebrayandev@gmail.com>
2024-07-23 15:27:31 +0300

    appointment page

---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,37 @@
+import PatientForm from "@/components/Form/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NewAppointment() {
+  return (
+    <div className="h-screen flex max-h-screen">
+      <div className=" container my-auto remove-scrollbar">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="h-10 mb-12 w-fit"
+          />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </div>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
+    </div>
+  );
+}
