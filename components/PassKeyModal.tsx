4f4a2acd893546b4a80d3e64df4b917985085132
RiugeBrian <riungebrayandev@gmail.com>
2024-07-25 10:13:49 +0300

    adding local storage

--- components/PassKeyModal.tsx
+++ components/PassKeyModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -12,12 +12,53 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { decryptKey, encryptKey } from "@/lib/utils";
 
 const PassKeyModal = () => {
   const router = useRouter();
   const [open, setOpen] = useState(false);
+  const [passkey, setPasskey] = useState("");
+  const [error, setError] = useState("");
+
+  const encryptedKey =
+    typeof window !== "undefined"
+      ? window.localStorage.getItem("accessKey")
+      : null;
+
+  useEffect(() => {
+    const accessKey = encryptedKey && decryptKey(encryptedKey);
+    if (accessKey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+      setOpen(false);
+      router.push("/admin");
+    } else {
+      setOpen(true);
+    }
+  }, [encryptedKey]);
+
+  const validatePasskey = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    if (passkey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+      const encryptedKey = encryptKey(passkey);
+
+      localStorage.setItem("accessKey", encryptedKey);
+      setOpen(false);
+    } else {
+      setError("Invalid Passkey. Please try again");
+    }
+  };
+
   const closeModal = () => {
     setOpen(false);
     router.push("/");
@@ -34,16 +75,42 @@ const PassKeyModal = () => {
               width={20}
               height={20}
               onClick={() => closeModal()}
+              className="cursor-pointer"
             />
           </AlertDialogTitle>
           <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            To access the admin page, please enter the passkey.
           </AlertDialogDescription>
         </AlertDialogHeader>
+        <div>
+          <InputOTP
+            maxLength={6}
+            value={passkey}
+            onChange={(value) => setPasskey(value)}
+          >
+            <InputOTPGroup className="shad-otp">
+              <InputOTPSlot className="shad-otp-slot" index={0} />
+              <InputOTPSlot className="shad-otp-slot" index={1} />
+              <InputOTPSlot className="shad-otp-slot" index={2} />
+              <InputOTPSlot className="shad-otp-slot" index={3} />
+              <InputOTPSlot className="shad-otp-slot" index={4} />
+              <InputOTPSlot className="shad-otp-slot" index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+
+          {error && (
+            <p className="shad-error text-14-regular mt-4 flex justify-center">
+              {error}
+            </p>
+          )}
+        </div>
         <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <AlertDialogAction
+            className="shad-primary-btn"
+            onClick={(e) => validatePasskey(e)}
+          >
+            Enter Admin Passkey
+          </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
